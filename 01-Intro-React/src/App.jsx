a376dc4bc837ad7cdafafd5b8fd4ecce5b656b83
África Centéotl Vázquez Martínez <afri.vaz.95@gmail.com>
2025-07-14 00:45:51 -0600

    Corrijo submódulos y subo carpetas como normales

---
+++ 01-Intro-React/src/App.jsx
@@ -0,0 +1,27 @@
+import { Card } from "./components/Card"
+
+
+function App() {
+  
+  return (
+    <>
+      <Card 
+        nombre="África Centéotl Vázquez Martínez" 
+        profesion="Diseñador web" 
+        descripcion="En busqueda de un trabajo "
+      />
+      <Card 
+        nombre="África Centéotl Vázquez Martínez" 
+        profesion="Lic.Enfermeria " 
+        descripcion="Buscando actualizarse y conseguir estudiante para su emprendimiento "
+      />
+      <Card 
+        nombre="África Centéotl Vázquez Martínez" 
+        profesion="Estudiante UNADM - Ingeniería en Desarrollo de Software y UNRC - Licenciatura en Contaduría y Finanzas" 
+        descripcion="Es una estudiante en la cual quiere ser alguien en la vida "
+      />
+    </>
+  )
+}
+
+export default App
